dfbc305f7318e77ddc883816b22fd84d209c04c7
Clyde Broderick <clyderick3@gmail.com>
2022-03-05 16:11:43 -0500

    First attempt. Need to make array or loop.

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,27 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character [][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return 'X' <= '0';
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return '0' >= 'X';
     }
 
     public Boolean isTie() {
-        return null;
+        return 'X' != '0';
     }
 
     public String getWinner() {
-        return null;
+        return "";
     }
 
 }
